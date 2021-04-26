727eeb6bb6f899cbc25eaa11bb2629646b82f2f0
Dewy <dewy@dewy.dev>
2021-04-26 20:14:02 +0100

    fix: cloudflare download issues

--- src/main/java/dev/dewy/mpm/MPM.java
+++ src/main/java/dev/dewy/mpm/MPM.java
@@ -34,6 +34,7 @@ import java.util.concurrent.Callable;
 )
 public class MPM implements Callable<Integer> {
     public static final String VERSION = "1.0.0";
+    public static final String DEFAULT_REPO = "https://mpm.dewy.dev";
 
     public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
@@ -78,7 +79,7 @@ public class MPM implements Callable<Integer> {
 
         try {
             if (BASIC_SETTINGS.createNewFile()) {
-                basicSettings = new BasicSettings("https://mpm.dewy.dev", false);
+                basicSettings = new BasicSettings(DEFAULT_REPO, false);
             } else {
                 basicSettings = SettingsManager.deserializeBasic();
             }
@@ -100,6 +101,10 @@ public class MPM implements Callable<Integer> {
         }
 
         SettingsManager.save();
+
+        if (basicSettings.getRepository().contains(DEFAULT_REPO)) {
+            System.setProperty("http.agent", "Chrome");
+        }
     }
 
     public static void main(String[] args) {
