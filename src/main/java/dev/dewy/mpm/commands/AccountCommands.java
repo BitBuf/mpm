c528fdee24fd4e87f3315099ef395d621a860f6a
Dewy <dewy@dewy.dev>
2021-04-28 21:35:10 +0100

    fix: account add command silliness

--- src/main/java/dev/dewy/mpm/commands/AccountCommands.java
+++ src/main/java/dev/dewy/mpm/commands/AccountCommands.java
@@ -33,8 +33,8 @@ public class AccountCommands {
 
             MPM.authSettings.getAccounts().add(new Account(
                     params[0],
-                    params[1],
                     params[2],
+                    params[1],
                     auth.getAccessToken(),
                     auth.getClientToken()
             ));
